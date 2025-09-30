509f5d931f004074ae2890aa8d75289454b42be6
zerOne-123 <528541096@qq.com>
2025-09-30 14:09:08 +0800

    refactor: 优化 Vite 配置文件结构

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from "node:url";
+// import monacoEditorEsmPlugin from "vite-plugin-monaco-editor-esm";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    // monacoEditorEsmPlugin({
+    //   languageWorkers: ["editorWorkerService", "typescript"],
+    // }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    reportCompressedSize: false,
+    outDir: "hiker-web-edit",
+  },
+});
